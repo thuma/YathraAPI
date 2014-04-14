7e88ac9b0821cf46faaf86372f10923f9e5fa885
Martin Harari Thuresson <martin.thure@gmail.com>
2014-04-14 06:18:10 +0200

    added swebus

--- tornadostart.py
+++ tornadostart.py
@@ -13,6 +13,7 @@ import xtr
 import mas
 import snalltaget
 import sj
+import swebus
 import os
 
 settings = {
@@ -37,7 +38,9 @@ application = tornado.web.Application([
     (r"/mas/", mas.Handler),
     (r"/mas/cache/", mas.CachePrint),
     (r"/xtr/", xtr.Handler),
-    (r"/xtr/cache/", xtr.CachePrint),    
+    (r"/xtr/cache/", xtr.CachePrint),
+    (r"/swebus/", swebus.Handler),
+    (r"/swebus/cache/", swebus.CachePrint),  
     (r"/snalltaget/", snalltaget.Handler),
     (r"/snalltaget/cache/", snalltaget.CachePrint),
     (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': os.getcwd()+'/static'})
