5333409fd7da5bce50b7c6459f65b4496a872ea3
Martin Thuresson <martin.thure@gmail.com>
2014-07-11 10:55:23 +0200

    added ul API start

---
+++ ul.py
@@ -0,0 +1,4 @@
+
+
+http://www.ul.se/JourneyService/JourneyService.svc/GetJourneys/?fromPointId=700578&fromPointType=0&toPointId=791305&toPointType=0&date=2014-07-11&time=10:50&directionType=0&noOfPagesBefore=0&noOfPagesAfter=0
+
